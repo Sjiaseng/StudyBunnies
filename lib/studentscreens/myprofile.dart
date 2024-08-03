2d6b3725834c4340fb35be728b217d56bc4ca16e
heloo1027 <huienloo03@gmail.com>
2024-08-03 11:01:09 +0800

    update - faq - student profile

--- lib/studentscreens/myprofile.dart
+++ lib/studentscreens/myprofile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
@@ -9,17 +10,228 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
+  final ImagePicker _picker = ImagePicker();
+  String? _pickedImagePath;
+  String? _selectedCountry;
+  String? _selectedRole;
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _pickedImagePath = pickedFile.path;
+      });
+      print('Picked image path: ${pickedFile.path}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: GestureDetector(
-        onTap: (){Navigator.pop(context);},
-        child: SizedBox(
-          width: 10.w,
-          height: 10.0.h,
-          child: Icon(Icons.arrow_back),
-        ), 
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(left: 7.w, top: 3.h),
+                    width: 10.w,
+                    height: 10.h,
+                    child: Icon(Icons.arrow_back, size: 20.sp),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 3.h, right: 8.w),
+                      child: Text(
+                        'My Profile',
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 2.h),
+            Center(
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: const AssetImage('images/profile.webp'),
+                    radius: 12.w,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 8.w,
+                      height: 8.w,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 0.w, right: 0.w),
+                        child: IconButton(
+                          icon: Icon(Icons.camera_alt,
+                              size: 4.5.w, color: Colors.white),
+                          onPressed: _pickImage,
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 2.h),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                "User ID: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                style: TextStyle(
+                  fontSize: 8.sp,
+                  color: Colors.grey,
+                ),
+                overflow: TextOverflow.ellipsis, // Handle long user ID
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 3.h),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Name',
+                    ),
+                  ),
+                  SizedBox(height: 2.h),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Contact Number',
+                    ),
+                  ),
+                  SizedBox(height: 2.h),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'E-mail',
+                    ),
+                    enabled: false,
+                  ),
+                  SizedBox(height: 2.h),
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Country',
+                    ),
+                    value: _selectedCountry,
+                    items: <String>['Option 1', 'Option 2', 'Option 3']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedCountry = newValue;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 2.h),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    enabled: false,
+                  ),
+                  SizedBox(height: 2.h),
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Role',
+                    ),
+                    value: _selectedRole,
+                    items: <String>['Option 1', 'Option 2', 'Option 3']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: null, // Make the dropdown not editable
+                  ),
+                  SizedBox(height: 5.h),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Save Changes pressed');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20.w, vertical: 2.h),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      minimumSize: const Size(double.infinity,
+                          50), // Ensures the button takes full width
+                    ),
+                    child: const Text(
+                      'Save Changes',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 2.h),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Reset Password pressed');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20.w, vertical: 2.h),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      minimumSize: const Size(double.infinity,
+                          50), // Ensures the button takes full width
+                    ),
+                    child: const Text(
+                      'Reset Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
